635ab2a688ae5c43ca02c5e9f40b7bdd47336f9f
vincanger <70215737+vincanger@users.noreply.github.com>
2023-10-24 12:49:07 +0200

    discord link never expires

--- src/server/actions.js
+++ src/server/actions.js
@@ -43,7 +43,7 @@ export const registerProject = async (registration, context) => {
       html: `<p>Hi ${registered.name}!</p><p>Thanks for registering for the Wasp SaaS-a-Thon! We are excited to see what you build!</p>
     <p>Remember, the hackathon officially starts on Sunday, October 22nd, and ends on Saturday, October 28th at 11:59 p.m.</p>
     <p>And don't forget to read the <a href='https://wasp-lang.notion.site/Wasp-SaaS-a-Thon-bd53e032d22f460d8578343f909f3bc4?pvs=4'> rules and instructions</a>. It's got a lot of helpful info there to help you build your app quickly & easily. </p>
-    <p>If you have any questions, reach out to us in our discord server: https://discord.gg/NvACCTMH</p>
+    <p>If you have any questions, reach out to us in our discord server: https://discord.gg/Y9XcgEPTTM</p>
     <p>Happy Hacking!</p>
     <p>-Vince & The Wasp Team</p>`,
     });
